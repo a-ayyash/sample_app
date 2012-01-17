a5096da6c22eb7cae06b8bdc8c11b2a6ae59ecef
Ahmad Ayyash <ayyash@blueprintlabs.com>
2012-01-18 00:02:49 +0200

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+ render_views #tell rspec explicitly to render the views so it can be sure they exist. 
+
+
+
+
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | Home")
+    end
+    
   end
 
   describe "GET 'contact'" do
@@ -14,6 +25,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => " | Contact")
+    end
+    
   end
 
+  describe "GET 'about'" do
+    it "returns https success" do
+     get 'about'
+     response.should be_success
+   end
+   
+   it "should have the right title" do
+     get 'about'
+     response.should have_selector("title", :content => " | About")
+   end
+   
+  end
 end
