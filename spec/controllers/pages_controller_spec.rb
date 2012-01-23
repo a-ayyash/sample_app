4e0962ed4a446570a99bc645e3ecac1dae79d813
Ahmad Ayyash <ayyash@blueprintlabs.com>
2012-01-23 16:33:40 +0200

    adding blueprint stylesheets and helpers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,9 @@ describe PagesController do
  render_views #tell rspec explicitly to render the views so it can be sure they exist. 
 
 
-
+  before(:each) do
+    @base_title = "Ruby On Rails Tutorials Sample App"
+  end
 
 
   describe "GET 'home'" do
@@ -15,7 +17,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => " | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
     
   end
@@ -28,11 +30,23 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => " | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
     
   end
 
+  describe "GET 'help'" do
+    it "should http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
+    end
+    
+  end
   describe "GET 'about'" do
     it "returns https success" do
      get 'about'
@@ -41,7 +55,7 @@ describe PagesController do
    
    it "should have the right title" do
      get 'about'
-     response.should have_selector("title", :content => " | About")
+     response.should have_selector("title", :content => @base_title + " | About")
    end
    
   end
