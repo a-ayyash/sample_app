9bfe59f4d2119b80bcea43d245d02daf629eac90
Ahmad Ayyash <ayyash@blueprintlabs.com>
2012-02-19 12:13:10 +0200

    deleting factory.rb

--- spec/factory.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :user do |user|
-  user.name                   "Ahmad Ayyash"
-  user.email                  "ahmad@ayyash.com"
-  user.password               "foobar"
-  user.password_confirmation  "foobar"
-end
